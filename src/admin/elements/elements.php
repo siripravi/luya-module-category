5100740db197adedbd261d5c29487c9b6b699471
provdigi <purnachandra.valluri@gmail.com>
2025-06-22 21:15:33 +0530

    fixes

--- src/admin/elements/elements.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'nodetitle' => function ($href, $name) {
-        return "<i class=\"material-icons\">add</i><a class=\"btn btn-primary\" href=\"{$href}\">{$name}</a>";
-    },
-
-];
